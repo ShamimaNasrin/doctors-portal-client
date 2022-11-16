f5cd7d4a36d8557feaaf2c7f83467236f8a634ee
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-16 20:10:58 +0600

    footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footerimg from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
-        </div>
+        <footer
+            style={{
+                background: `url(${footerimg})`,
+                backgroundSize: 'cover'
+            }}
+            className="p-10">
+            <div className='footer grid justify-between'>
+                <div className='w-44'>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Emergency Checkup</Link>
+                    <Link to="/" className="link link-hover">Monthly Checkup</Link>
+                    <Link to="/" className="link link-hover">Weekly Checkup</Link>
+                    <Link to="/" className="link link-hover">Deep Checkup</Link>
+                </div>
+                <div className='w-44'>
+                    <span className="footer-title">ORAL HEALTH</span>
+                    <Link to="/" className="link link-hover">Fluoride Treatment</Link>
+                    <Link to="/" className="link link-hover">Cavity Filling</Link>
+                    <Link to="/" className="link link-hover">Teath Whitening</Link>
+
+                </div>
+                <div className='w-44'>
+                    <span className="footer-title">OUR ADDRESS</span>
+                    <p>New York - 101010 Hudson</p>
+
+                </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2022 All Rights Reserved</p>
+            </div>
+        </footer>
     );
 };
 
