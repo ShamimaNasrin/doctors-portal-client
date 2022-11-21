1dfa00227de72728346c6b1ce3055967e59a1c56
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-21 20:25:00 +0600

    load user specific booking by email address

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -8,7 +8,7 @@ const DashboardLayout = () => {
             <Navbar></Navbar>
             <div className="drawer drawer-mobile">
                 <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content">
+                <div className="drawer-content bg-slate-100">
                     <Outlet></Outlet>
                 </div>
                 <div className="drawer-side">
