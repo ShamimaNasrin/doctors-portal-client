3aa7e1915fe5dd6e72f234d64dbf91532b19fdf9
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-24 00:28:59 +0600

    display card error

--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -1,7 +1,8 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckoutForm = () => {
+    const [cardError, setCardError] = useState('');
 
     const stripe = useStripe();
     const elements = useElements();
@@ -11,31 +12,53 @@ const CheckoutForm = () => {
         if (!stripe || !elements) {
             return
         }
+
+
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return;
+        }
+
+        //to create the method of payment 
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        if (error) {
+            console.log(error);
+            setCardError(error.message);
+        }
+        else {
+            setCardError('');
+        }
     }
 
     return (
         <div>
-             <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+            <form onSubmit={handleSubmit} className='shadow-xl p-6 bg-white rounded-md m-6'>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-sm mt-4 btn-primary text-white' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+
+                <p className="text-red-500 mt-4">{cardError}</p>
+            </form>
         </div>
     );
 };
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,6 +1,8 @@
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
 
 const Payment = () => {
     const booking = useLoaderData();
@@ -12,9 +14,16 @@ const Payment = () => {
     console.log(stripePromise);
 
     return (
-        <div>
+        <div className='p-7'>
             <h3 className="text-3xl">Payment for {treatment}</h3>
             <p className="text-xl">Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
+
+            {/* call the checkoutform */}
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
